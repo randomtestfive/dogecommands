f59a51196ea400789903ce36633eb6fd723df2e1
randomtestfive <randomtestfive@gmail.com>
2016-04-18 19:46:18 -0400

    Remove debugging comments

--- dogecommands.ps1
+++ dogecommands.ps1
@@ -28,8 +28,7 @@ function wow {
 	{
 		$tempdoge = $doge;
 		$color = Get-Random -input $colors -count 3;
-	
-		#$phrases = "very powershell","much command","such windoge","so ascii","many code";
+
 		$dogephrase = $sophrases,$sophrases;
 		$phrase = Get-Random -input $phrases -count 2;
 		$dogephrase[0] = Get-Random -input $phrase[0];
@@ -37,10 +36,6 @@ function wow {
 		
 		$lines = ($tempdoge -split '[\n]');
 		$line = Get-Random -input $lines[4..($lines.length - 3)] -count 2;
-		#while(($line[0] -ge $lines[$lines.length - 2]) -or ($line[1] -ge $lines[$lines.length - 2]))
-		#{
-		#	$line = Get-Random -input $lines -count 2;
-		#}
 		$phrasenum = 0;
 		$linenum = 0;
 		foreach($dogeline in $lines)
@@ -53,7 +48,6 @@ function wow {
 				$frag1 = -join $dogearray[0..$phrasestart];
 				$frag2 = "wow";
 				$frag3 = -join $dogearray[($phraseend + 1)..($dogeline.length - 1)];
-				#Write-Color -Text $frag1,$frag2,$frag3 -Color "yellow",$color[$phrasenum],"yellow";
 				Write-Host $frag1 -NoNewLine -ForegroundColor "yellow";
 				Write-Host $frag2 -NoNewLine -ForegroundColor $color[$phrasenum];
 				Write-Host $frag3 -ForegroundColor "yellow";
@@ -65,7 +59,6 @@ function wow {
 			
 				if(($dogeline -eq $line[0]) -or ($dogeline -eq $line[1]))
 				{
-					#Write-Host $dogeline;
 					$dogearray = [char[]]$dogeline;
 					$phrasestart = Get-Random -minimum 0 -maximum ($dogearray.length - 1);
 					$phraseend = $dogephrase[$phrasenum].length + $phrasestart;
@@ -85,16 +78,8 @@ function wow {
 				}
 			}
 		}
-		
-		#Write-Host $phrase1 -foregroundcolor $color[0];
-		#sleep -m (1 * $speedmultiply);
-		#Write-Host $phrase2 -foregroundcolor $color[1];
-		#sleep -m (1 * $speedmultiply);
-		#Get-Content "C:\Users\taylo\Documents\doge.txt" | Write-Host -foregroundcolor "yellow";
-		#Write-Host $doge -foregroundcolor "yellow";
+
 		echo "";
-		#sleep -m (2 * $speedmultiply);
-		#Write-Host "                              wow" -foregroundcolor $color[2];
 		sleep -m (1 * $speedmultiply);
 		$loop--;
 	}
@@ -165,11 +150,9 @@ function doge {
 	$phrases = $sophrases,$veryphrases,$muchphrases,$suchphrases,$manyphrases,$howphrases; 
 
 	while($loop -ne 0)
-	{
-		
+	{		
 		$color = Get-Random -input $colors -count 3;
-	
-		#$phrases = "very powershell","much command","such windoge","so ascii","many code";
+
 		$phrase = Get-Random -input $phrases -count 2;
 		$phrase1 = Get-Random -input $phrase[0];
 		$phrase2 = Get-Random -input $phrase[1];
@@ -178,7 +161,6 @@ function doge {
 		sleep -m (1 * $speedmultiply);
 		Write-Host $phrase2 -foregroundcolor $color[1];
 		sleep -m (1 * $speedmultiply);
-		#Get-Content "C:\Users\taylo\Documents\doge.txt" | Write-Host -foregroundcolor "yellow";
 		Write-Host $doge -foregroundcolor "yellow";
 		echo "";
 		sleep -m (2 * $speedmultiply);
